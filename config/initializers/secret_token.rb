fb519281515e12e494ead98451f7cb603098bba7
Rajeev.Reddy <rajeev.reddy.d@gmail.com>
2013-03-18 03:32:52 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ebuzz::Application.config.secret_token = '29b227ea9ec9445bd89f3239cdef808df52d3fec98f7d768424c386be7b5c954ce9f528ae1cc03f28b098ad45f20c6faf0347f13b3964947c7f58d6ba5ee2c54'
