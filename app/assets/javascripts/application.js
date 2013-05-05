ae6bb6ff669e284b964d68f496aac26fbcde766b
D. Rajeev. Reddy <rajeev.reddy.d@gmail.com>
2013-05-05 06:45:04 +0530

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,44 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function() {
+
+  var tot = $("#num").html();
+	var i = 0;
+	for(i=0;i<=tot;i++){
+		$("#flip" + i).click(function(){
+		var type = $(this).attr('rel');
+		var name = $(this).text();
+		var dest = $(this).attr("href");
+		//alert(type +' / '+ name +' / '+ dest);
+			if($(this).attr('class')=='test'){
+				$(this).removeClass("test");
+				$(this).addClass('done');
+				$.ajax({ 
+				  type: 'POST', 
+				  url: '/test', 
+				  data: {'info' : name , 'type' : type }, 
+				  success: function(data){
+				  	$(dest + ' .modal-body .content').hide();
+				  	var actdata = data.aInfo.teaser;
+				  	//alert(actdata);
+				  	//var parts = actdata.split("</p><p>");
+				  	//alert(parts[0]);
+					//for (i = 0, l = parts.length; i < l; i += 2) {
+					//    $("#" + parts[i]).text(parts[i + 1]);
+					//}
+				  	$(dest + ' .modal-body .content').html(actdata);
+
+				  	if(data.hasOwnProperty('aVideo')){
+				  		var url = data.aVideo.link;
+				  		var video = "<br/><hr/><center><embed src='"+url+"' type='application/x-shockwave-flash' allowfullscreen='true' width='360' height='222' title='Adobe Flash Player'></center><hr/>";
+				 		$(dest + ' .modal-body .content').append(video);
+				  	}
+				 	$(dest + ' .modal-body .content').fadeIn();
+			    } 
+			 });
+			};
+		});
+	};
+});
